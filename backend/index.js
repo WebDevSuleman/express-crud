57a9a57c9246a77987a314026fbe87c12a6e8baf
WebDevSuleman <suleman848586@gmail.com>
2025-04-26 22:42:49 +0500

    Added dynamic port for deployment

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000; 
+
 app.use(
   cors({
     origin: "http://localhost:5173",
